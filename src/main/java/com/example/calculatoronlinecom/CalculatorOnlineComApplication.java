6771c073254ddd9018d99bc66a38510d538309ab
Z3Z4 <vladislav.rusin@gmail.com>
2021-09-08 21:13:19 +0300

    Создал новый репозиторий для спринг части проекта

---
+++ src/main/java/com/example/calculatoronlinecom/CalculatorOnlineComApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatoronlinecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorOnlineComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorOnlineComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculatoronlinecom/CalculatorOnlineComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatoronlinecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorOnlineComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
