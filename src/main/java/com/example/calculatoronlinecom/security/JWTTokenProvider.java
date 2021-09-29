f6d08291baa777f815dc0e3e86129abf088a9bd5
Z3Z4 <bushwalker34>
2021-09-29 23:42:15 +0300

    Написал комментарии для нашего разработчика Дмитрия

--- src/main/java/com/example/calculatoronlinecom/security/JWTTokenProvider.java
+++ src/main/java/com/example/calculatoronlinecom/security/JWTTokenProvider.java
@@ -13,26 +13,26 @@ import java.util.Map;
 public class JWTTokenProvider {
     public static final Logger LOG = LoggerFactory.getLogger(JWTTokenProvider.class);
 
-    public String generateToken(Authentication authentication){
-        User user = (User) authentication.getPrincipal();
-        Date now = new Date(System.currentTimeMillis());
-        Date expiryDate = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME);
+    public String generateToken(Authentication authentication){         //Здесь генерируется токен для входа на сайт
+        User user = (User) authentication.getPrincipal();               //в котором есть конкретный пользователь
+        Date now = new Date(System.currentTimeMillis());                //текущая дата для создания токена
+        Date expiryDate = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME);          //Время истечения токена
         String userId = Long.toString(user.getId());
 
-        Map<String, Object> claimsMap = new HashMap<>();
+        Map<String, Object> claimsMap = new HashMap<>();    //создается новая хашмапа, в которую добавляются поля ниже
         claimsMap.put("id", userId);
         claimsMap.put("username", user.getEmail());
         claimsMap.put("firstname", user.getName());
         claimsMap.put("lastname", user.getLastname());
 
-        return Jwts.builder()
+        return Jwts.builder()                           //Здесь строится и шифруется токен исходя из наших данных и хешмапы сверху
                 .setSubject(userId)
                 .addClaims(claimsMap)
                 .setIssuedAt(now)
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)   //алгоритм ассиметричного шифрования, можно использовать  HS512, RS512, ES256, ES512
                 .compact();
     }
-    public boolean validateToken(String token){
+    public boolean validateToken(String token){                 //здесь проверяется полученный токен от пользователя, если не ок - выдает ошибку в лог
         try {
             Jwts.parser()
                     .setSigningKey(SecurityConstants.SECRET)
@@ -48,12 +48,12 @@ public class JWTTokenProvider {
         }
 
     }
-    public Long getUserIdFromToken(String token){
+    public Long getUserIdFromToken(String token){           //здесь мы получаем токен юзера и извлекаем из него необходимые нам данные, которые будем использовать в дальнейшем
         Claims claims = Jwts.parser()
                 .setSigningKey(SecurityConstants.SECRET)
                 .parseClaimsJws(token)
                 .getBody();
-        String id = (String) claims.get("id");
+        String id = (String) claims.get("id");              //но здесь нам почему-то нужен только айдишник, который видимо будет куда-то выводиться, например в окно успешной авторизации
         return Long.parseLong(id);
 
     }
