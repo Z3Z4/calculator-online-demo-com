535e7ba5efd711b5013d951061737f005df563d3
Z3Z4 <bushwalker34>
2021-10-05 22:40:57 +0300

    Создал и описал UserExistException.java, UserService.java

---
+++ src/main/java/com/example/calculatoronlinecom/services/UserService.java
@@ -0,0 +1,46 @@
+package com.example.calculatoronlinecom.services;
+
+import com.example.calculatoronlinecom.entity.User;
+import com.example.calculatoronlinecom.entity.enums.ERole;
+import com.example.calculatoronlinecom.exceptions.UserExistException;
+import com.example.calculatoronlinecom.payload.request.SignupRequest;
+import com.example.calculatoronlinecom.repository.IUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class UserService {
+    public static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+    private final IUserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(IUserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(SignupRequest userIn) {
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setName(userIn.getFirstname());
+        user.setLastname(userIn.getLastname());
+        user.setUsername(userIn.getUsername());
+        user.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        user.getRole().add(ERole.ROLE_USER);
+
+        try {
+            LOG.info("Saving user {}", userIn.getEmail());
+            return userRepository.save(user);
+
+
+        } catch (Exception e) {
+            LOG.error("Error during registration. {}", e.getMessage());
+            throw new UserExistException("The user " + user.getUsername() + "already exist. Please check credential");
+
+        }
+
+
+    }
+}
