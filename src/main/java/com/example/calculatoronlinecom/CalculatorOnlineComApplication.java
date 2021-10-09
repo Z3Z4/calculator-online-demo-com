bf42cb57909859a07574de5c4ee9418b75469873
tacdelta <tacdelta@protonmail.com>
2021-10-09 23:27:27 +0300

    Spr 04-Security Hotfix to UserSevice - bCryptEncoder

--- src/main/java/com/example/calculatoronlinecom/CalculatorOnlineComApplication.java
+++ src/main/java/com/example/calculatoronlinecom/CalculatorOnlineComApplication.java
@@ -2,6 +2,8 @@ package com.example.calculatoronlinecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CalculatorOnlineComApplication {
@@ -10,4 +12,10 @@ public class CalculatorOnlineComApplication {
         SpringApplication.run(CalculatorOnlineComApplication.class, args);
     }
 //hello world
+
+    //TODO Это костыль, найти как можно исправить (UserService.java)
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
