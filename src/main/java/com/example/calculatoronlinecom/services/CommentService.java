f478e251ea24b74180357725fd5897326c5cd35b
Z3Z4 <bushwalker34>
2021-10-24 10:57:00 +0300

    Создал и описал CommentService.java Описали возможность создания комментариев =)

---
+++ src/main/java/com/example/calculatoronlinecom/services/CommentService.java
@@ -0,0 +1,71 @@
+package com.example.calculatoronlinecom.services;
+
+
+import com.example.calculatoronlinecom.dto.CommentDTO;
+import com.example.calculatoronlinecom.entity.Comment;
+import com.example.calculatoronlinecom.entity.Post;
+import com.example.calculatoronlinecom.entity.User;
+import com.example.calculatoronlinecom.exceptions.PostNotFoundException;
+import com.example.calculatoronlinecom.repository.ICommentRepository;
+import com.example.calculatoronlinecom.repository.IPostRepository;
+import com.example.calculatoronlinecom.repository.IUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService {
+    public static final Logger LOG = LoggerFactory.getLogger(CommentService.class);
+
+    private final ICommentRepository commentRepository;
+    private final IPostRepository postRepository;
+    private final IUserRepository userRepository;
+
+    @Autowired
+    public CommentService(ICommentRepository commentRepository, IPostRepository postRepository, IUserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Comment saveComment(Long postId, CommentDTO commentDTO, Principal principal) {
+        User user = getUserByPrincipal(principal);
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("Post cannot be found for username: " + user.getEmail()));
+
+        Comment comment = new Comment();
+        comment.setPost(post);
+        comment.setUserId(user.getId());
+        comment.setUsername(user.getUsername());
+        comment.setMessage(commentDTO.getMessage());
+
+        LOG.info("Saving comment for Post: {}", post.getId());
+        return commentRepository.save(comment);
+    }
+
+    public List<Comment> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("Post cannot be found"));
+        List<Comment> comments = commentRepository.findAllByPost(post);
+
+        return comments;
+    }
+
+    public void deleteComment(Long commentId) {
+        Optional<Comment> comment = commentRepository.findById(commentId);
+        comment.ifPresent(commentRepository::delete);
+    }
+
+
+    private User getUserByPrincipal(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username " + username));
+    }
+}
