56792350be5d5dd8ea468d1528a9fa4864db5c1a
Z3Z4 <bushwalker34>
2021-11-26 21:36:34 +0300

    попыточка

---
+++ src/app/service/token-storage.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenStorageService {
+
+  constructor() {
+  }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string {
+    return <string>sessionStorage.getItem(TOKEN_KEY); //костыль с приведением типов к стрингу
+  }
+
+  public saveUser(user: any): void {  //TODO костыль с приведением user к any
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): any {
+    return JSON.parse(<string>sessionStorage.getItem(USER_KEY)); //костыль с приведением типов к стрингу
+  }
+
+  logOut(): void {
+    window.sessionStorage.clear();
+    window.location.reload();
+  }
+}
