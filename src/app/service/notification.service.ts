ff0262ce7526506fb8ef974ba0b653d1e9c8b393
Z3Z4 <bushwalker34>
2021-11-27 22:02:46 +0300

    описали comment.service.ts, image-upload.service.ts, notification.service.ts, post.service.ts, user.service.ts урок 4-8, 08 папка

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private snackbar: MatSnackBar) { }
+
+  public showSnackBar(message: string): void {
+    this.snackbar.open(message, "", {   //todo debug normally, action у него был null, но он подчеркивается красным, поэтому мы сделали просто пустой экшн
+      duration: 2000
+    });
+  }
 }
